94396b673d7f71b3a828689580aa23dbe212f08b
Aqheel <aqheelsharief@gmail.com>
2026-09-08 22:09:26 +0530

    third problem

---
+++ solutions/operators/arithmetic/3 simple interest.py
@@ -0,0 +1,5 @@
+p = int(input("Enter principal amount: "))
+r = int(input("Enter rate of interest: "))
+t = int(input("Enter amount of time: "))
+
+print(f'Calculated simple interest is {p*r*t/100:0.2f}')
