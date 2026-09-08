9aeb2547819b4f8d6f05a2ac27d9909e333fe91f
Aqheel <aqheelsharief@gmail.com>
2026-09-08 22:01:14 +0530

    first commit

---
+++ solutions/operators/arithmetic/basic.py
@@ -0,0 +1,8 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+print(f'sum of {a} and {b} is {a+b}')
+print(f'difference of {a} and {b} is {a-b}')
+print(f'product of {a} and {b} is {a*b}')
+print(f'qoutient of {a} and {b} is {a/b}')
+print(f'remainder of {a} and {b} is {a%b}')
