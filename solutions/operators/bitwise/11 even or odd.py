8bf3f601ca1a81e788ff71535b63b09ef9240c1b
unknown <amasl@gmail.com>
2026-09-09 11:01:19 +0530

    new

--- solutions/operators/bitwise/11 even or odd.py
+++ solutions/operators/bitwise/11 even or odd.py
@@ -1,2 +1,2 @@
 m = int(input("Enter a number: "))
-print(f'number is {'odd' if m&1!=0 else "even"})
+print(f'number is {'odd' if m&1!=0 else "even"}')
---
+++ solutions/operators/bitwise/12 swap.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+m = int(input("Enter another number: "))
+
+n = n^m
+m = n^m
+n = n^m
+print(f'After swapping: n = {n}, m = {m}')
