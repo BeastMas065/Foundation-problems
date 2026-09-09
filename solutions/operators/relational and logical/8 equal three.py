9ca61c0318195c90a91eac1c72bc52803d4847d9
BeastMas <aqheelsharief@gmail.com>
2026-09-09 10:09:54 +0530

    Add script to check if three numbers are equal

---
+++ solutions/operators/relational and logical/8 equal three.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if (a == b and b == c) :
+    print(f'{a}, {b} and {c} are equal')
+else:
+    print(f'{a}, {b} and {c} are not equal')
